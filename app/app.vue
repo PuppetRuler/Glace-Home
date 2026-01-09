f7f24ac5c2ffb25b0221072aa23412a9424cfc14
PuppetMaster <ziyelixiang@gmail.com>
2026-01-09 15:08:59 +0800

    submit

--- app/app.vue
+++ app/app.vue
@@ -37,11 +37,11 @@ useSeoMeta({
   titleTemplate: '%s | Glace.top',
   
   // 核心：搜索结果下方的灰色介绍文字
-  description: "Glace'Home,个人主页,集成了博客,网盘,联系方式等,更多功能正在开发中...欢迎各位朋友一起交流学习！",
+  description: 'Glace Home,个人主页,集成了博客,网盘,联系方式等,更多功能正在开发中...欢迎各位朋友一起交流学习！',
   
   // 社交媒体分享预览（建议同步配置）
   ogTitle: 'Glace - 傀儡师的个人主页',
-  ogDescription: "Glace'Home,个人主页,集成了博客,网盘,联系方式等,更多功能正在开发中...欢迎各位朋友一起交流学习！",
+  ogDescription: 'Glace Home,个人主页,集成了博客,网盘,联系方式等,更多功能正在开发中...欢迎各位朋友一起交流学习！',
   ogImage: '/home.png', // 分享到社交软件时的预览图
   twitterCard: 'summary_large_image',
 })
